39693935f398f05e29ef80d9cce0eca077de1f63
jontysh4w <79010302+jontysh4w@users.noreply.github.com>
2024-01-28 17:47:22 +0000

    hex_to_bin testing

--- hex_convert.js
+++ hex_convert.js
@@ -15,8 +15,8 @@ function hexbin(hexIn){
     return x;
 }
  
-function call_hex_bin(){
-    var binStr= document.getElementById("hexInput").value;
-    const result = binStr;
-    document.getElementById("binOutput").value= result;
+function call_hex_bin() {
+    var hexStr = document.getElementById("hexInput").value;
+    var binStr = parseInt(hexStr, 16).toString(2);
+    document.getElementById("binOutput").value = binStr;
 }
